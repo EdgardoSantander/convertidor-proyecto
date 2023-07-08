6117c3620533ab85a4ad5b4de780f6e56e7493d3
Edgardo <Edsaag95@outlook.com>
2023-07-07 23:02:37 -0600

    creacion de repositorio

---
+++ modelo/Moneda.java
@@ -0,0 +1,21 @@
+package convertidor.modelo;
+
+public class Moneda {
+	private String nombre;
+	private String pais;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public String getPais() {
+		return pais;
+	}
+	
+	public void setNombre(String n) {
+		this.nombre = n;
+	}
+	public void setPais(String p) {
+		this.pais = p;
+	}
+	
+}
---
+++ test/Principal.java
@@ -0,0 +1,9 @@
+package convertidor.test;
+import convertidor.modelo.Moneda; 
+public class Principal {
+	public static void main(String[] args) {
+		Moneda moneda = new Moneda();
+		moneda.setNombre("Peso");
+		moneda.setPais("Mexico");
+	}
+}
